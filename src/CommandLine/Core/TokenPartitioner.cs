d0274a79c3dbaac711d4835c320a4ff6cef6fb7e
Ladislav Šeps <ladislav.seps@newtontech.cz>
2024-10-01 14:38:06 +0200

    net8 & lib update

--- src/CommandLine/Core/TokenPartitioner.cs
+++ src/CommandLine/Core/TokenPartitioner.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright 2005-2015 Giacomo Stelluti Scala & Contributors. All rights reserved. See License.md in the project root for license information.
 
+using CSharpx;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using CommandLine.Infrastructure;
-using CSharpx;
+using ReferenceEqualityComparer = CommandLine.Infrastructure.ReferenceEqualityComparer;
 
 namespace CommandLine.Core
 {
